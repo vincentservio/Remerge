cb75dbb9facd96718ee5f111a5b08f05ccfcbed9
vincentservio <vincentservio@gmail.com>
2021-02-15 21:29:28 -0500

    handle submit*

--- src/Form.js
+++ src/Form.js
@@ -7,4 +7,33 @@ export default class Form extends Component {
       bills: "",
     };
   }
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.props.handleonSubmit(this.state);
+    this.setState({
+      bills: "",
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      bills: e.target.value,
+    });
+  };
+  render() {
+    return (
+      <form>
+        <label>New Bill:</label>
+        <input
+          type="text"
+          value={this.state.bills}
+          onChange={this.handleChange}
+        />
+        <button type="submit" onClick={this.handleSubmit}>
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
